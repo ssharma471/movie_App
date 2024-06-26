337f11476c5d213a6e7970146e8c767c3493fe21
ssharma471 <sidhantsharmavlog@gmail.com>
2024-06-25 23:20:02 -0400

    Added about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-5xl mx-auto p-8 space-y-8">
+      <h1 className="text-2xl font-dedium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+        doloribus inventore dolorum, repudiandae sit earum deleniti explicabo
+        fugiat, quisquam quod omnis aliquid vitae ratione asperiores eius. Iste
+        commodi quaerat harum!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
+        obcaecati voluptatem repellendus esse alias praesentium, fugiat totam
+        porro ad? Praesentium ipsam eligendi cumque aliquam quisquam inventore
+        tenetur consequatur voluptas numquam temporibus, alias perferendis
+        aspernatur adipisci omnis tempora iusto deleniti! Aliquam amet ipsa quis
+        fuga cumque. Voluptatibus, nostrum asperiores ratione inventore
+        repudiandae voluptate eaque corrupti, mollitia amet vel eos harum modi
+        beatae quam rerum! Necessitatibus quam nam placeat iure molestiae, saepe
+        accusamus tempora ea minima iusto nobis, assumenda ut dolor esse odio ab
+        quis. Delectus omnis quisquam nesciunt magnam praesentium, error dolor
+        quaerat eos eveniet reprehenderit, aspernatur sequi id atque quo
+        distinctio quam tempore. In placeat veniam rerum mollitia saepe maxime
+        deserunt, quaerat, nihil inventore molestias nesciunt quasi eaque,
+        laudantium perspiciatis tempore voluptatibus neque consequatur ad
+        obcaecati. Enim, vitae aspernatur! Et, soluta architecto? Dolor,
+        obcaecati. Quas eum excepturi cupiditate laborum magnam voluptatem neque
+        distinctio non quisquam ipsa minus obcaecati, reprehenderit delectus,
+        ut, ducimus cumque illum. Libero enim voluptate et minima adipisci quis
+        molestiae cum aliquam dolorum? Quo, obcaecati accusamus. Dolor ipsum
+        nobis aperiam nam magnam, ab laborum libero, sit quam ipsam eum
+        reiciendis blanditiis reprehenderit rem minus laudantium assumenda
+        illum. Ducimus officia nesciunt esse error molestiae, blanditiis quas
+        hic velit, animi illo asperiores voluptate ratione aut sed fuga nemo.
+        Ipsum repellendus veniam nulla ipsa iure laborum nemo, aut at corrupti
+        reiciendis temporibus dicta nihil tempora, sed voluptates accusamus!
+        Sapiente doloremque maiores at sunt, ab libero esse voluptatum
+        perspiciatis commodi accusantium quia cum iste eligendi temporibus porro
+        deleniti nostrum, quo provident aperiam natus incidunt dolor distinctio
+        facilis? Suscipit esse similique vitae odit ipsam porro iure laudantium
+        non quis ipsum error dolores fugiat tempora harum soluta illo aliquid ut
+        ducimus eius ullam sit, odio molestiae cum saepe. Excepturi quibusdam
+        atque inventore provident veritatis, consectetur laborum! Voluptatum
+        officiis, enim porro veniam sunt facere hic facilis vitae eius illum
+        voluptas tempora adipisci reprehenderit est aperiam, quam odit commodi,
+        neque cumque voluptatem quia? Magnam quaerat fugiat soluta quas,
+        delectus, magni voluptas mollitia quos recusandae nesciunt rerum vel,
+        doloremque est beatae ducimus dolorum reprehenderit quae dolor. Sint
+        provident amet earum cum delectus ea facere vel nulla, veritatis beatae
+        nostrum quo, ullam illum perferendis assumenda. Ex praesentium placeat
+        ipsa suscipit atque fugiat. Adipisci repellat repellendus excepturi hic
+        quidem maxime est assumenda itaque consectetur, cupiditate atque
+        incidunt tenetur, placeat accusamus quos, facilis dolorem quia magni
+        sunt! Tempore nemo quis, ratione recusandae deleniti dolore quod,
+        doloribus ea quibusdam iure esse?
+      </p>
+    </div>
+  );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 import MenuItem from "./MenuItem";
-import {AiFillHome} from 'react-icons/ai';
-import {BsFillInfoCircleFill} from 'react-icons/bs';
-import Link from 'next/link'
-
+import { AiFillHome } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import Link from "next/link";
+import DarkMode from "./DarkMode";
 
 export default function Header() {
   return (
     <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
       <div className="flex gap-10">
-        <MenuItem title="Home" address="/" Icon={AiFillHome}/>
-        <MenuItem title="About" address="/about" Icon={BsFillInfoCircleFill}/>
-
+        <MenuItem title="Home" address="/" Icon={AiFillHome} />
+        <MenuItem title="About" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
 
-      <Link href={'/'} className="flex gap-10 items-center">
-        <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">IMDB</span>
-        <span className="text-xl hidden sm:inline">Clone</span>
-
-
-
-      </Link>
+      <div className="flex items-center gap-4">
+        <DarkMode />
+        <Link href={"/"} className="flex gap-10 items-center">
+          <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">
+            IMDB
+          </span>
+          <span className="text-xl hidden sm:inline">Clone</span>
+        </Link>
+      </div>
     </div>
   );
 }
